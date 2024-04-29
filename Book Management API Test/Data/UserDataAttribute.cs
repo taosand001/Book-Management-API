cc2c235990c526679dc51c556fedd46ae797b720
Povka <f.povilas@gmail.com>
2024-04-29 21:07:49 +0300

    Created few Tests

---
+++ Book Management API Test/Data/UserDataAttribute.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+
+namespace Book_Management_API_Test.Data
+{
+    public class UserDataAttribute : AutoDataAttribute
+    {
+        public UserDataAttribute() : base(() =>
+        {
+            var fixture = new Fixture();
+            fixture.Customizations.Add(new UserSpecimenBuilder());
+            return fixture;
+        })
+        { }
+    }
+}

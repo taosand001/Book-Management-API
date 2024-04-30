6c9e9d04eca35de7179f9bb838ab4dab8c0b5401
Taofeek <taofeekdboy@yahoo.com>
2024-04-30 20:51:05 +0300

    add recommendation

---
+++ Book Management API/Service/RecommendationService.cs
@@ -0,0 +1,15 @@
+﻿using Book_Management_API.Database;
+
+namespace Book_Management_API.Service
+{
+    public class RecommendationService
+    {
+        private readonly BookContext _context;
+
+        public RecommendationService(BookContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
